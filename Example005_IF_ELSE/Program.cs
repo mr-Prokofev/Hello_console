f5ea9dd01e87fad362308da64e84c7bb81825d0f
mr Prokofev <prav.1@list.ru>
2023-03-05 16:26:59 +0300

    add Ex005_if_else

---
+++ Example005_IF_ELSE/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string u_name = Console.ReadLine();
+if (u_name.ToLower() == "artem")
+{
+    Console.WriteLine("Yes, my Lord!");
+}
+else
+{
+    Console.Write("Buy!");
+    Console.WriteLine(u_name);
+}
