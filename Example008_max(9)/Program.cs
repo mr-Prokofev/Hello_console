f06873c133aeff1697d1c03996c8dc5c90b53199
mr Prokofev <prav.1@list.ru>
2023-03-11 16:57:48 +0300

    add Ex008 max()

---
+++ Example008_max(9)/Program.cs
@@ -0,0 +1,10 @@
+﻿int Maximum (int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int max1 = Maximum(1,2,3);
+Console.WriteLine(max1);
