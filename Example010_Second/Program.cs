b72548435a31de781d1670463c9891cf00385f83
mr Prokofev <prav.1@list.ru>
2023-03-11 20:44:23 +0300

    add Ex9 and Ex13

---
+++ Example010_Second/Program.cs
@@ -0,0 +1,9 @@
+﻿int SecondDigit(int number)
+{
+    int result = number >= 100 && number < 1000 ? number/10%10 : -1;
+    return result;
+}
+
+int num = new Random().Next(100, 1000);
+Console.WriteLine(num);
+Console.WriteLine(SecondDigit(num) != -1 ? SecondDigit(num): "Проверьте условие");
---
+++ Example013_3num/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int count = 0;
+int secondDigit = -1;
+while (num >= 100) 
+    {
+        secondDigit = num % 10;
+        num/=10;
+        count++; 
+    }
+Console.WriteLine(secondDigit == -1 ? "No":secondDigit);
